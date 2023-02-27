db88c72e1733036342473412c58f42feb8e1bb92
Emilio López <redorav@gmail.com>
2023-02-27 19:10:08 +0000

    Rename to m_data for consistency with other containers

--- include/crstl/array.h
+++ include/crstl/array.h
@@ -11,28 +11,28 @@ namespace crstl
 	{
 	public:
 
-		crstl_constexpr T* begin() { return m_contents; }
-		crstl_constexpr const T* begin() const { return m_contents; }
-		crstl_constexpr const T* cbegin() const { return m_contents; }
+		crstl_constexpr T* begin() { return m_data; }
+		crstl_constexpr const T* begin() const { return m_data; }
+		crstl_constexpr const T* cbegin() const { return m_data; }
 
-		crstl_constexpr T* end() { return m_contents + N; }
-		crstl_constexpr const T* end() const { return m_contents + N; }
-		crstl_constexpr const T* cend() const { return m_contents + N; }
+		crstl_constexpr T* end() { return m_data + N; }
+		crstl_constexpr const T* end() const { return m_data + N; }
+		crstl_constexpr const T* cend() const { return m_data + N; }
 
 		crstl_constexpr bool empty() const { return N == 0; }
 		crstl_constexpr size_t size() const { return N; }
 		crstl_constexpr size_t max_size() const { return N; }
 
-		crstl_constexpr T* data() { return m_contents; }
-		crstl_constexpr T* const data() const { return m_contents; }
+		crstl_constexpr T* data() { return m_data; }
+		crstl_constexpr T* const data() const { return m_data; }
 
-		crstl_constexpr T& operator[](size_t i) { return m_contents[i]; }
-		crstl_constexpr const T& operator[](size_t i) const { return m_contents[i]; }
+		crstl_constexpr T& operator[](size_t i) { return m_data[i]; }
+		crstl_constexpr const T& operator[](size_t i) const { return m_data[i]; }
 
-		crstl_constexpr T& at(size_t i) { return m_contents[i]; }
-		crstl_constexpr const T& at(size_t i) const { return m_contents[i]; }
+		crstl_constexpr T& at(size_t i) { return m_data[i]; }
+		crstl_constexpr const T& at(size_t i) const { return m_data[i]; }
 
 		// Public to allow braced initialization
-		T m_contents[N ? N : 1];
+		T m_data[N ? N : 1];
 	};
 }
