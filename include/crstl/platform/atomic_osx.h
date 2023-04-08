43859102c750c6a401094eb6a07d9e1de913da30
Emilio López <redorav@gmail.com>
2023-04-08 01:06:02 +0100

    Commit changes before update

--- include/crstl/platform/atomic_osx.h
+++ include/crstl/platform/atomic_osx.h
@@ -1,51 +1,58 @@
 #pragma once
 
+//#include <libkern/OSAtomic.h>
+
+extern "C"
+{
+    char OSAddAtomic8(int amount, volatile char* address);
+};
+
 // Storage
 
-#define crstl_atomic_store8(target, value) OSAtomicCompareAndSwap32(value, target)
+#define crstl_atomic_store8(target, value) 0//OSAtomicCompareAndSwap32(value, target)
 
-#define crstl_atomic_store16(target, value) OSAtomicCompareAndSwap32(value, target)
+#define crstl_atomic_store16(target, value) 0//OSAtomicCompareAndSwap32(value, target)
 
-#define crstl_atomic_store32(target, value) OSAtomicCompareAndSwap32(value, target)
+#define crstl_atomic_store32(target, value) 0//OSAtomicCompareAndSwap32(value, (crstl::int32_t*)target)
 
-#define crstl_atomic_store64(target, value) OSAtomicCompareAndSwap64(value, target)
+#define crstl_atomic_store64(target, value) 0//OSAtomicCompareAndSwap64(value, target)
 
 // Addition
 
-#define crstl_atomic_add8(target, value) OSAtomicAdd32Barrier(value, target)
+#define crstl_atomic_add8(target, value) OSAddAtomic8(value, (volatile char*)target)
 
-#define crstl_atomic_add16(target, value) OSAtomicAdd32Barrier(value, target)
+#define crstl_atomic_add16(target, value) 0//OSAtomicAdd32Barrier(value, target)
 
-#define crstl_atomic_add32(target, value) OSAtomicAdd32Barrier(value, target)
+#define crstl_atomic_add32(target, value) 0//__c11_atomic_fetch_add(target, value, __ATOMIC_SEQ_CST)
 
-#define crstl_atomic_add64(target, value) OSAtomicAdd64Barrier(value, target)
+#define crstl_atomic_add64(target, value) 0//OSAtomicAdd64Barrier(value, target)
 
 // And
 
-#define crstl_atomic_and8(target, value) OSAtomicAnd32Barrier(value, target)
+#define crstl_atomic_and8(target, value) 0//OSAtomicAnd32Barrier(value, target)
 
-#define crstl_atomic_and16(target, value) OSAtomicAnd32Barrier(value, target)
+#define crstl_atomic_and16(target, value) 0//OSAtomicAnd32Barrier(value, target)
 
-#define crstl_atomic_and32(target, value) OSAtomicAnd32Barrier(value, target)
+#define crstl_atomic_and32(target, value) 0//OSAtomicAnd32Barrier(value, target)
 
-#define crstl_atomic_and64(target, value) OSAtomicAnd64Barriers(value, target)
+#define crstl_atomic_and64(target, value) 0//OSAtomicAnd64Barriers(value, target)
 
 // Or
 
-#define crstl_atomic_or8(target, value) OSAtomicOr32Barrier(value, target)
+#define crstl_atomic_or8(target, value) 0//OSAtomicOr32Barrier(value, target)
 
-#define crstl_atomic_or16(target, value) OSAtomicOr32Barrier(value, target)
+#define crstl_atomic_or16(target, value) 0//OSAtomicOr32Barrier(value, target)
 
-#define crstl_atomic_or32(target, value) OSAtomicOr32Barrier(value, target)
+#define crstl_atomic_or32(target, value) 0//OSAtomicOr32Barrier(value, target)
 
-#define crstl_atomic_or64(target, value) OSAtomicOr64Barrier(value, target)
+#define crstl_atomic_or64(target, value) 0//OSAtomicOr64Barrier(value, target)
 
 // Xor
 
-#define crstl_atomic_xor8(target, value) OSAtomicXor32Barrier(value, target)
+#define crstl_atomic_xor8(target, value) 0//OSAtomicXor32Barrier(value, target)
 
-#define crstl_atomic_xor16(target, value) OSAtomicXor32Barrier(value, target)
+#define crstl_atomic_xor16(target, value) 0//OSAtomicXor32Barrier(value, target)
 
-#define crstl_atomic_xor32(target, value) OSAtomicXor32Barrier(value, target)
+#define crstl_atomic_xor32(target, value) 0//OSAtomicXor32Barrier(value, target)
 
-#define crstl_atomic_xor64(target, value) OSAtomicXor64Barrier(value, target)
+#define crstl_atomic_xor64(target, value) 0//OSAtomicXor64Barrier(value, target)
