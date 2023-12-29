a4e4bef2c428d9b9e5843f5eddba2d0d119e066d
Emilio López <redorav@gmail.com>
2023-12-29 11:59:08 +0000

    Add common win32 typedefs

---
+++ include/crstl/platform/common_win32.h
@@ -0,0 +1,17 @@
+#pragma once
+
+namespace crstl
+{
+	typedef unsigned long DWORD;
+	typedef long LONG;
+	typedef int BOOL;
+	typedef char CHAR;
+
+	typedef CHAR* LPSTR;
+	typedef const CHAR* LPCSTR;
+
+	typedef unsigned __int64 ULONG_PTR, *PULONG_PTR;
+
+	typedef LONG HRESULT;
+	typedef void* HANDLE;
+};
