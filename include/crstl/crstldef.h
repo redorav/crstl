071bc4efee80c862b3acb97654d955a552cfc38c
Emilio López <redorav@gmail.com>
2023-12-29 12:11:10 +0000

    Fix module declaration

--- include/crstl/crstldef.h
+++ include/crstl/crstldef.h
@@ -4,9 +4,13 @@
 
 #include "crstl/type_builtins.h"
 
+#if defined(CRSTL_MODULE_DECLARATION)
+import <cstdint>;
+import <cstddef>;
+#elif defined(CRSTL_INITIALIZER_LISTS)
 #include <cstdint>
-
 #include <cstddef>
+#endif
 
 #if !defined(CRSTL_CHAR8_TYPE)
 
