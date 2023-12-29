e815a52d7acc0f95c34e2e953acddb449e71d5c6
Emilio López <redorav@gmail.com>
2023-12-29 20:34:40 +0000

    Add more windows forward declarations

--- include/crstl/platform/common_win32.h
+++ include/crstl/platform/common_win32.h
@@ -2,13 +2,18 @@
 
 namespace crstl
 {
+	typedef unsigned int UINT;
 	typedef unsigned long DWORD;
 	typedef long LONG;
 	typedef int BOOL;
 	typedef char CHAR;
 
+	typedef const CHAR* LPCCH, *PCCH;
 	typedef CHAR* LPSTR;
 	typedef const CHAR* LPCSTR;
+
+	typedef wchar_t WCHAR;
+	typedef WCHAR* LPWSTR;
 
 	typedef unsigned __int64 ULONG_PTR, *PULONG_PTR;
 
