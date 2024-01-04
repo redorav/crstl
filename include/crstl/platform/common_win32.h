6068009d07cd47afb93386e1ec5279ad787d0f2e
Emilio López <redorav@gmail.com>
2024-01-04 17:15:04 +0000

    Add more forward-declared windows types

--- include/crstl/platform/common_win32.h
+++ include/crstl/platform/common_win32.h
@@ -2,11 +2,33 @@
 
 namespace crstl
 {
-	typedef unsigned int UINT;
-	typedef unsigned long DWORD;
-	typedef long LONG;
+	typedef char                CHAR;
+	typedef signed char         INT8;
+	typedef unsigned char       UCHAR;
+	typedef unsigned char       UINT8;
+	typedef unsigned char       BYTE;
+	typedef short               SHORT;
+	typedef signed short        INT16;
+	typedef unsigned short      USHORT;
+	typedef unsigned short      UINT16;
+	typedef unsigned short      WORD;
+	typedef int                 INT;
+	typedef signed int          INT32;
+	typedef unsigned int        UINT;
+	typedef unsigned int        UINT32;
+	typedef long                LONG;
+	typedef unsigned long       ULONG;
+	typedef unsigned long       DWORD;
+	typedef __int64             LONGLONG;
+	typedef __int64             LONG64;
+	typedef signed __int64      INT64;
+	typedef unsigned __int64    ULONGLONG;
+	typedef unsigned __int64    DWORDLONG;
+	typedef unsigned __int64    ULONG64;
+	typedef unsigned __int64    DWORD64;
+	typedef unsigned __int64    UINT64;
+
 	typedef int BOOL;
-	typedef char CHAR;
 
 	typedef const CHAR* LPCCH, *PCCH;
 	typedef CHAR* LPSTR;
