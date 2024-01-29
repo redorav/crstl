dc9d07ea53d99a1e8857a7ea1d1376d1f1113889
Emilio López <redorav@gmail.com>
2024-01-29 17:44:03 +0000

    Add is_debugger_present for OSX

--- include/crstl/debugging.h
+++ include/crstl/debugging.h
@@ -11,6 +11,10 @@ extern "C"
 
 	__declspec(dllimport) crstl::BOOL IsDebuggerPresent();
 };
+#elif defined(CRSTL_OS_OSX)
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/sysctl.h>
 #endif
 
 crstl_module_export namespace crstl
@@ -38,6 +42,26 @@ crstl_module_export namespace crstl
 	{
 #if defined(CRSTL_COMPILER_MSVC)
 		return IsDebuggerPresent();
+#elif defined(CRSTL_OS_OSX)
+		// https://developer.apple.com/library/archive/qa/qa1361/_index.html
+
+		// Initialize the flags so that, if sysctl fails for some reason, we get a predictable result
+		struct kinfo_proc info;
+		info.kp_proc.p_flag = 0;
+
+		// Initialize mib, which tells sysctl the info we want, in this case we're looking for information about a specific process ID
+		int mib[4];
+		mib[0] = CTL_KERN;
+		mib[1] = KERN_PROC;
+		mib[2] = KERN_PROC_PID;
+		mib[3] = getpid();
+
+		size_t size = sizeof(info);
+		int sysctl_return = sysctl(mib, sizeof(mib) / sizeof(*mib), &info, &size, NULL, 0);
+		crstl_assert(sysctl_return == 0);
+
+		// We're in the debugger if the P_TRACED flag is set
+		return ((info.kp_proc.p_flag & P_TRACED) != 0);
 #else
 		return 0;
 #endif
