93a14b73faf8e21f557747d49413990c8f6ca61e
Emilio López <redorav@gmail.com>
2024-02-13 21:17:29 +0000

    Try defining our own ptrdiff_t again

--- include/crstl/crstldef.h
+++ include/crstl/crstldef.h
@@ -16,6 +16,35 @@ namespace crstl
 {
 	typedef decltype(nullptr) nullptr_t;
 	typedef decltype(sizeof(1)) size_t;
+
+	template<int SizeT>
+	struct pointer_types
+	{
+		typedef int64_t ptrdiff_t;
+		typedef int64_t intptr_t;
+		typedef uint64_t uintptr_t;
+	};
+
+	template<>
+	struct pointer_types<4>
+	{
+		typedef int32_t ptrdiff_t;
+		typedef int32_t intptr_t;
+		typedef uint32_t uintptr_t;
+	};
+
+	template<>
+	struct pointer_types<2>
+	{
+		typedef int16_t ptrdiff_t;
+		typedef int16_t intptr_t;
+		typedef uint16_t uintptr_t;
+	};
+
+	typedef decltype(sizeof(1)) size_t;
+	typedef pointer_types<sizeof(size_t)>::ptrdiff_t ptrdiff_t;
+	typedef pointer_types<sizeof(size_t)>::intptr_t intptr_t;
+	typedef pointer_types<sizeof(size_t)>::uintptr_t uintptr_t;
 };
 
 #if !defined(CRSTL_CHAR8_TYPE)
