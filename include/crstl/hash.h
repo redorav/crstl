f04fec1203372a3d98cca0117688a46dd512d69b
Emilio López <redorav@gmail.com>
2024-05-31 01:14:54 +0100

    Improve the pointer hash function
    It doesn't need to be perfect but it needs to be fast and break the correlation between aligned pointers and lack of entropy in the lower bits

--- include/crstl/hash.h
+++ include/crstl/hash.h
@@ -8,7 +8,32 @@ namespace crstl
 {
 	template<typename T> struct hash;
 
-	template <typename T> struct hash<T*> { size_t operator()(T* p) const { return static_cast<size_t>(uintptr_t(p)); } };
+	// Pointers tend to be correlated by being aligned so they'll typically end up in the same buckets and cause many collisions.
+	// This is a relatively fast way to mix them up a bit without being too expensive. Other approaches such as removing the bottom
+	// bits are very cheap but cause many collisions, and better hashes like Wang or 
+	// https://stackoverflow.com/questions/664014/what-integer-hash-function-are-good-that-accepts-an-integer-hash-key
+	template <typename T> struct hash<T*>
+	{
+		size_t operator()(T* ptr) const
+		{
+			uintptr_t uptr = (uintptr_t)ptr;
+
+			crstl_constexpr_if(sizeof(T*) == 8)
+			{
+				uptr = (uptr ^ (uptr >> 30)) * 0xbf58476d1ce4e5b9;
+				uptr = (uptr ^ (uptr >> 27)) * 0x94d049bb133111eb;
+				uptr = uptr ^ (uptr >> 31);
+			}
+			else
+			{
+				uptr = ((uptr >> 16) ^ uptr) * 0x45d9f3b;
+				uptr = ((uptr >> 16) ^ uptr) * 0x45d9f3b;
+				uptr = (uptr >> 16) ^ uptr;
+			}
+
+			return static_cast<size_t>(uptr);
+		}
+	};
 
 	template<> struct hash<bool> { size_t operator()(bool value) const { return static_cast<size_t>(value); } };
 
