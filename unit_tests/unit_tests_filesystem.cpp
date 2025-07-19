322c64822424259b3d4890115cd1e35a34be2ba0
Emilio López <redorav@gmail.com>
2025-07-19 18:43:29 +0100

    Create directory in case it doesn't exist

--- unit_tests/unit_tests_filesystem.cpp
+++ unit_tests/unit_tests_filesystem.cpp
@@ -20,6 +20,7 @@ void RunUnitTestsFilesystem()
 	begin_test("filesystem");
 	{
 		crstl::path temp_path = crstl::temp_directory_path() / "crstl_temp";
+		crstl::create_directories(temp_path.c_str());
 
 		crstl::path temp_file_path = temp_path / "temp_file.txt";
 		crstl::path temp_file_copy_path = temp_path / "temp_file_copy.txt";
